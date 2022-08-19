625e6c69728ce27bff7ed2763d0d28b9d724dd0e
Masahiro Odakura <wendy.odakura@gmail.com>
2022-08-19 20:13:21 +0900

    グーグルのアカウント情報をgitignore

---
+++ config/firebase.php
@@ -0,0 +1,8 @@
+<?php
+return [
+
+    'firebase-private-key-id' => env('FIREBASE_PRIVATE_KEY_ID'),
+    'firebase-private-key' => env('FIREBASE_PRIVATE_KEY'),
+    'firebase-client-email' => env('FIREBASE_CLIENT_EMAIL'),
+    'firebase-client-id' => env('FIREBASE_CLIENT_ID')
+];
