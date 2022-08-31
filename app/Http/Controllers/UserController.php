dd467ac0a5191daad5b07fd84bcb2a93de022ab8
Masahiro Odakura <wendy.odakura@gmail.com>
2022-08-31 19:04:32 +0900

    gameAdd修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -214,11 +214,11 @@ class UserController extends Controller
         ]);
         
         // attack配列作成
-        $validAttacks = $this->createAttackLoop($request->valid_attacks, false, true, false);
-        $competitorValidAttacks = $this->createAttackLoop($request->competitor_valid_attacks, true, true, false);
-        $competitorAttacks = $this->createAttackLoop($request->competitor_attacks, true, false, false);
-        $attacks = $this->createAttackLoop($request->attacks, false, false, false);
-        $defeatAttacks = $this->createAttackLoop($request->defeat_attacks, false, false, true);
+        (!$request->valid_attacks) ? $validAttacks = $this->createAttackLoop($request->valid_attacks, false, true, false) : $validAttacks = [];
+        (!$request->competitor_valid_attacks) ? $competitorValidAttacks = $this->createAttackLoop($request->competitor_valid_attacks, true, true, false) : $competitorValidAttacks = [];
+        (!$request->competitor_attacks) ? $competitorAttacks = $this->createAttackLoop($request->competitor_attacks, true, false, false) : $competitorAttacks = [];
+        (!$request->attacks) ? $attacks = $this->createAttackLoop($request->attacks, false, false, false) : $attacks = [];
+        (!$request->defeat_attacks) ? $defeatAttacks = $this->createAttackLoop($request->defeat_attacks, false, false, true) : $defeatAttacks = [];
         $totalAttacksArray = array_merge($validAttacks, $competitorValidAttacks, $competitorAttacks, $attacks, $defeatAttacks);
 
         // foul配列作成
