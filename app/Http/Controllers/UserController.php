2a108e6af870bab97fc8c44c7d32357a9ca50757
Masahiro Odakura <wendy.odakura@gmail.com>
2022-09-01 17:27:23 +0900

    getGame修正

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -186,6 +186,8 @@ class UserController extends Controller
                 }
             }
         }
+        
+        array_multisort($attack_list, SORT_DESC);
 
         $response_data = [
             "date" => date('Y/m/d', strtotime($game->updated_at)),
@@ -252,18 +254,18 @@ class UserController extends Controller
 
     private function createAttackLoop($array, $competitor, $valid, $defeat){
         $attacks = [];
-        foreach($array as $attack){
-            // Attempt to read property "skill_id" on intだとさ
-            if($attack != null){
-                $attack_array = array(
-                    "skill_id" => $attack,
-                    "competitor" => $competitor,
-                    "valid" => $valid,
-                    "defeat" => $defeat
-                );
-                array_push($attacks, $attack_array);
+            foreach($array as $attack){
+                // Attempt to read property "skill_id" on intだとさ
+                if($attack != null){
+                    $attack_array = array(
+                        "skill_id" => $attack,
+                        "competitor" => $competitor,
+                        "valid" => $valid,
+                        "defeat" => $defeat
+                    );
+                    array_push($attacks, $attack_array);
+                }
             }
-        }
         return $attacks;
     }
 
